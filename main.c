4829c876285d429eebc5be054b1528e2ef5172d9
Samuel Worash Getaneh <samuelworash@gmail.com>
2025-11-21 12:12:40 +0300

    basic graphics

--- main.c
+++ main.c
@@ -1,14 +1,21 @@
-
 #include "raylib.h"
+#include <stdlib.h>
+#define WIDTH 800
 
+// Function inits
 void update(); // Update and draw one frame
+void check_game(); // Check game state
 
-int main()
-{
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+// Game variables
+int game[3][3] = {0}; // 0 - empty, 1 - X, 2 - O
+int gameState = 0; // 0 - not playing, 1 - playing, 2 - tie, 3 - X won, 4 - O won
+int turn = 1; // 1 - X, 2 - 0
 
-    InitWindow(screenWidth, screenHeight, "raylib");
+int main() {
+    const int screenWidth = WIDTH + 20;
+    const int screenHeight = WIDTH + 80;
+
+    InitWindow(screenWidth, screenHeight, "tic tac toe");
 
     SetTargetFPS(60);
 
@@ -24,11 +31,24 @@ int main()
 }
 
 // Update and draw game frame
-void update()
-{
+void update() {
     BeginDrawing();
         ClearBackground(RAYWHITE);
-        DrawText("This is a raylib example", 10, 40, 20, DARKGRAY);
-        DrawFPS(10, 10);
+        DrawText("Tic Tac Toe", 10, 10, 30, DARKGRAY);
+        if(gameState == 0) DrawText("X to start. Click to make your move.", 10, 40, 20, DARKGRAY);
+        else if(gameState == 1) DrawText((turn == 1 ? "X's turn" : "O's turn"), 10, 40, 20, DARKGRAY);
+        else if(gameState == 2) DrawText("It's a tie!", 10, 40, 20, ORANGE);
+        else if(gameState == 3) DrawText("X won! Click anywhere to play again.", 10, 40, 20, GREEN);
+        else if(gameState == 3) DrawText("O won! Click anywhere to play again.", 10, 40, 20, GREEN);
+        DrawFPS(WIDTH - 65, 10);
+        DrawRectangle(10, 70, WIDTH, WIDTH, LIGHTGRAY); // Background
+        DrawRectangle(10 + WIDTH / 3, 70, 5, WIDTH, DARKGRAY); // vertical bar 1
+        DrawRectangle(10 + (WIDTH * 2) / 3, 70, 5, WIDTH, DARKGRAY); // vertical bar 2
+        DrawRectangle(10, 70 + WIDTH / 3, WIDTH, 5, DARKGRAY); // horizontal bar 1
+        DrawRectangle(10, 70 + (WIDTH * 2) / 3, WIDTH, 5, DARKGRAY); // horizontal bar 2
     EndDrawing();
+}
+
+void game_check() {
+
 }
